8f4103be2ce6140f553da7b90f72b9daa4175759
ExeQun <abramkindaniil31@gmail.com>
2026-05-17 16:00:02 +0300

    Настроена админ-панель для всех моделей

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Author, Category, Tag, Article, Comment
 
-# Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['user', 'bio']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'category', 'status', 'published_at']
+    list_filter = ['status', 'category']
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author_name', 'article', 'created_at', 'is_visible']
--- subscribers/admin.py
+++ subscribers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Subscriber, QuestionToEditor
 
-# Register your models here.
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ['email', 'subscribed_at', 'is_active']
+
+@admin.register(QuestionToEditor)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'asked_at', 'is_answered']
